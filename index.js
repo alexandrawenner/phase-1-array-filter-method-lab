9c71e31eea96e089b0dd0005f5ecac0ef208a1b4
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-05-22 19:18:50 -0400

    commit tree 1a70836560ef5fadd3ff8e9f415b17ae71180c6c

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution here
+//TypeError: Bobby is not a function
+// function findMatching(drivers, name) {
+//     drivers.filter(name)
+//         return name
+// }
+
+
+// function findMatching(drivers, name) {
+//     for (const match of drivers){
+//         if (drivers === name) {
+//             return match
+//         }
+//     }
+// }
+
+
+function findMatching(drivers,name) {
+     return drivers.filter(function(member){
+        return  name === member || member === name.toLowerCase()
+     })
+}
+
+
+
+function fuzzyMatch(drivers, letterMatch) {
+    return drivers.filter(function(firstLetter){
+        return firstLetter.startsWith(letterMatch)
+    }) 
+}
+
+
+
+
+function matchName(drivers, nameMatch) {
+    return drivers.filter(function(FullNameMatch) {
+        return nameMatch === FullNameMatch.name
+    })
+}
+
+
+
+// function matchName(drivers, cb) {
+//     const newNames = []
+//     for(const same of drivers) {
+//         if (cb(same)){
+//             newNames.push(same)
+//         }
+//     }
+//     return newNames
+// }
